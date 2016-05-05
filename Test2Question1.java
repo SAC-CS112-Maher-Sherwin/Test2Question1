2d8b169c2118f2ce7fde46765d4fd97e64a56a76
SAC-CS112-Maher-Sherwin <sherwin.maher@gmail.com>
2016-05-04 20:14:24 -0700

    Added files via upload

---
+++ Test2Question1.java
@@ -0,0 +1,13 @@
+package test2question1;
+
+public class Test2Question1 {
+
+    public static void main(String[] args) {
+
+        int dice = 3;
+        System.out.println(Dice.Throw(dice));
+        
+    }
+    
+}
+
